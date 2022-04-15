b798346f1a238c79be7321c67f8af1aba61d4c92
Hector Perez <Hector.Perez@neudesic.com>
2022-04-15 14:59:37 -0700

    many changes

---
+++ src/components/TodoApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class TodoApp extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Hello form Create React App</h1>
+                <p>I am a React Component!</p>
+            </div>
+        );
+    }
+}
+export default TodoApp;
